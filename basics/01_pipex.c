16e994a75450e98d6c3b9d30eac95a3952a8d1e6
Diego Camilo Silva Gomez <dsilva-g@c4r3s1.42urduliz.com>
2023-09-29 18:11:43 +0200

    upload: 00 - 04

---
+++ basics/01_pipex.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+/*
+ * Two process to the power of n time fork() is called.
+ * 2^n of process.
+ * In this example there will 16 prints.
+ */
+
+int	main(int ac, char *ag[])
+{
+	(void)ac;
+	(void)ag;
+	printf("There will be 16 I\n");
+	fork();
+	fork();
+	fork();
+	fork();
+	printf("I\n");
+}
