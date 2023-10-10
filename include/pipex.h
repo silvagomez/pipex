ab9653e49e181c88d44d0674adbba995e87a5c6a
Diego Camilo Silva Gomez <dsilva-g@c4r5s5.42urduliz.com>
2023-10-10 19:39:43 +0200

    update

--- include/pipex.h
+++ include/pipex.h
@@ -6,7 +6,7 @@
 /*   By: dsilva-g <dsilva-g@student.42urduli>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 12:35:08 by dsilva-g          #+#    #+#             */
-/*   Updated: 2023/10/09 16:03:29 by dsilva-g         ###   ########.fr       */
+/*   Updated: 2023/10/10 19:34:30 by dsilva-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,19 @@
 
 # include <fcntl.h>
 # include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
 # include <sys/wait.h>
 
 // Allowed libraries
 # include "../libft/libft.h"
 
+typedef struct s_pipex
+{
+	int		fd[2];
+	pid_t	pid;
+	int		file;
+
+}				t_pipex;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,31 +6,56 @@
 /*   By: dsilva-g <dsilva-g@student.42urduli>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 12:16:27 by dsilva-g          #+#    #+#             */
-/*   Updated: 2023/10/09 15:59:07 by dsilva-g         ###   ########.fr       */
+/*   Updated: 2023/10/10 19:36:44 by dsilva-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#incude "pipex.h"
+#include "pipex.h"
+
+void	error_terminate(char *s)
+{
+	perror(s);
+	exit(1);
+}
 
 /*
  * execve(const char *path, char *const argv[], char *const envp[]);
  */
-int	main(int ac, char *av[])
+/*
+size_t	validate_argv(char *av[], char *env[])
 {
-	int fd[2];
-	int pid;
 
-	if (pipe(fd) < 0)
-		exit(1);
-	pid = fork();
-	if (pid < 0)
-		exit(1);
-	if (pid == 0)
+}
+*/
+int	main(int ac, char *av[], char *env[])
+{
+	t_pipex	px;
+
+	if (ac != 5)
+		error_terminate("Error, required format: < file1 cmd1 | cmd2 > file2");
+/*
+	if (validate_argv(av, env) == 0)
+		error_terminate("Invalid argument: ");
+*/
+	if (pipe(px.fd) < 0)
+		error_terminate("Pipe: ");
+	px.pid = fork();
+	if (px.pid < 0)
+		error_terminate("Fork: ");
+	if (px.pid == 0)
 	{
+		close(px.fd[0]);
+		px.file = open("infile", O_RDONLY);
+		if (px.file < 0)
+			error_terminate("Open: ");
+		dup2(px.file, STDOUT_FILENO);
+		execve("/usr/bin/grep", av, env);
+		close(px.fd[1]);
 	}
 	else
 	{
 		wait(NULL);
+		ft_printf("Success\n");
 	}
 	return (0);
 }
